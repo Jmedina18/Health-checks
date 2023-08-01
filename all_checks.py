f01532d343b70820468546742929e3d2c89928cf
Javier <jmedinaalmendares@gmail.com>
2023-08-01 16:30:50 -0700

    Cree una función recursiva para verificar si el disco está lleno, Tambien itera sobre una lista de mensajes para evitar la  duplicacion de codigo y permite mostar mas de un error

--- all_checks.py
+++ all_checks.py
@@ -19,16 +19,25 @@ def check_disk_full(disk, min_gb, min_percent):
         return True
     return False
 
-def main():
-     if check_reboot():
-         print("Pending Reboot.")
-         sys.exit(1)
-     if check_disk_full(disk = "/", min_gb = 2, min_percent = 10):
-         print("Disk Full :(")
-         sys.exit(1)
+def check_root_full():
+    """Returns True if the root partition is full, false otherwise."""
+    return check_disk_full(disk="/",min_gb=2,min_percent=10)
 
-     print("I've check reboots and disk space and = Everything is Ok :)")
-     sys.exit(0)
+def main():
+    checks = [
+        (check_reboot, "Pending Reboot :("),
+        (check_root_full, "Root partition full :(")
+    ]
+    Everything_ok = True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            Everything_ok = False
+    if not Everything_ok:
+        sys.exit(1)
+    print("Everything is Ok :)")
+    sys.exit(0)
+
 
 
 
